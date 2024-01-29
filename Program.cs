7721798696818b4ca8ba491e9b0f59bc9afac046
Endruman <hayllihuk@gmail.com>
2024-01-29 21:55:23 +0300

    Задача 1 выполнена и upgrade bin/ & obj/

--- Program.cs
+++ Program.cs
@@ -2,7 +2,17 @@
 // на вход число и проверяет, кратно ли оно
 // одновременно 7 и 23
 
-int num_1 = 7;
-int num_2 = 23;
+// int num_1 = 7;
+// int num_2 = 23;
 
-Console.WriteLine("Введите число: ");
+// Console.Write("Введите число: ");
+// int user_num = Convert.ToInt32(Console.ReadLine());
+
+// if ((user_num % num_1 == 0) && (user_num % num_2 == 0))
+// {
+//     Console.WriteLine($"Число {user_num} кратно");
+// }
+// else
+// {
+//     Console.WriteLine($"Число {user_num} не кратно");
+// }
