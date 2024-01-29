3538592e8aea7cf71b34fba815f82b15751f5322
Endruman <hayllihuk@gmail.com>
2024-01-29 22:31:34 +0300

    Задача 2 решена и upgrade bin & obj

--- Program.cs
+++ Program.cs
@@ -16,3 +16,39 @@
 // {
 //     Console.WriteLine($"Число {user_num} не кратно");
 // }
+
+
+// Задача 2: Напишите программу, которая принимает
+// на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠
+// 0 и выдаёт номер координатной четверти плоскости,
+// в которой находится эта точка.
+
+// Console.Write("Введите координату X: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату Y: ");
+// int y = Convert.ToInt32(Console.ReadLine());
+
+// if (x == 0 || y == 0)
+// {
+//     Console.WriteLine("Координата не должна равняться 0");
+// }
+// else
+// {
+//     if (x > 0 && y > 0)
+//     {
+//         Console.WriteLine("1");
+//     }
+//     if (x > 0 && y < 0)
+//     {
+//         Console.WriteLine("2");
+//     }
+//     if (x < 0 && y < 0)
+//     {
+//         Console.WriteLine("3");
+//     }
+//     if (x < 0 && y > 0)
+//     {
+//         Console.WriteLine("4");
+//     }
+// }
+
